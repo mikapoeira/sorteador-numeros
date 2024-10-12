38873d221f0c871a78d7ada9e0c4b06fbed06925
Mikael Rizzuto <mikael.r.joaquim@gmail.com>
2024-10-12 11:14:30 -0300

    Botão reiniciar

--- app.js
+++ app.js
@@ -18,7 +18,26 @@ function sortear(){
 
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados} </label>`
+    alterarStatusBotao();
+}
 
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
+}
+
+function alterarStatusBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    if(botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado')
+    }
 }
 
 function obterNumeroAleatorio(min,max){
